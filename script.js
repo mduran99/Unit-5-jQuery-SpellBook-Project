21e0b0bcf0406c2ceca941bc144730e32f9126cf
mduran99 <47435025+mduran99@users.noreply.github.com>
2019-05-07 14:44:17 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,20 @@
+//Spell #1
+$("#revelio").click(function() {
+$(".nasty").toggle(); 
+});
+
+
+
+//Spell #2
+
+$("#Patronum").click(function() {
+$(".trump").toggle(); 
+});
+
+
+
+//Spell #3
+
+$("#Disappear").click(function() {
+$(".donut").toggle(); 
+});
